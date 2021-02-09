07d44160994e613b8d141a835b152bfc5d21aab1
arcticmonkey07 <arcticmonkey07@gmail.com>
2021-02-09 17:08:35 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app app>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar >
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item>
+          <v-btn>
+            <v-list-item-icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>title</v-list-item-title>
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
+
+        <v-list-item>
+          <v-btn>
+            <v-list-item-icon>
+              <v-icon>mdi-account-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>title</v-list-item-title>
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
+
+        <v-list-item>
+          <v-btn>
+            <v-list-item-icon>
+              <v-icon>mdi-exit-to-app</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>title</v-list-item-title>
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-card color="grey lighten-4" flat height="200px" tile>
+      <v-toolbar dense>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn icon to="/registration">
+          <v-icon>mdi-account-plus</v-icon>
+        </v-btn>
+        <v-btn icon to="/login">
+          <v-icon>mdi-exit-to-app</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <router-view></router-view>
+    </v-card>
+  </v-app>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   store,
   render: h => h(App)
 }).$mount("#app");
